2e30c7a338ca7c217f1422dddf5338da82202369
john-mckillip <john0mckillip@gmail.com>
2025-12-30 20:02:43 -0600

    Started working on the new block for technologies.

---
+++ IceNineMedia.Core/Features/Blocks/TechnologiesBlockViewModel.cs
@@ -0,0 +1,13 @@
+﻿using IceNineMedia.Core.Features.Shared.Models;
+using UContentMapper.Core.Models.Attributes;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using static IceNineMedia.Core.Features.Shared.AppConstants;
+
+namespace IceNineMedia.Core.Features.Blocks
+{
+    [MapperConfiguration(SourceType = typeof(IPublishedElement), ContentTypeAlias = ContentTypeAliases.TechnologiesBlock)]
+    public class TechnologiesBlockViewModel : BaseBlockModel
+    {
+        public IEnumerable<string> Technologies { get; set; } = [];
+    }
+}
